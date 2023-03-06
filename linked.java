bffa5f155069fdfffd9c555ad21bdc68034c9f83
Heraj25 <herajgantyada@gmail.com>
2023-03-06 13:14:15 +0530

    practice on linkedlist

---
+++ linked.java
@@ -0,0 +1,55 @@
+
+
+
+
+class Node{
+    int data;
+    Node next;
+}
+
+class LinkedList{
+
+    Node head;
+
+    public void insert(int data){
+        Node new_node = new Node();
+
+        new_node.data = data;
+
+
+        if(head == null){
+            head = new_node;
+        }
+        else{
+            Node n = head;
+            while(n.next != null){
+                n = n.next;
+            }
+            n.next = new_node;
+        }
+    }
+
+    public void show(){
+        Node new_node = head;
+        while(new_node.next != null){
+            System.out.print(new_node.data+" ");
+            new_node = new_node.next;
+        }
+
+        System.out.print(new_node.data);
+    }
+
+    
+} 
+
+
+class linked{
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insert(600);
+        list.insert(400);
+
+        list.show();
+    }
+}
